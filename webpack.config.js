c43c726e1b37e0d14b41b6a45c7037b59cfde6b5
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-05-09 21:31:40 +0300

    Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, 'src', 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+};
