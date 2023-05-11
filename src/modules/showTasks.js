c55634e6fe68d67fc8cea85c569823973bb3a482
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-05-11 14:36:28 +0300

    Fix linter errors

--- src/modules/showTasks.js
+++ src/modules/showTasks.js
@@ -10,7 +10,7 @@ const showTasks = (tasksDiv) => {
             <input type="checkbox" class="list__item-checkbox"${task.done ? ' checked' : ''}>
             <span class="list__item-text list-text">${task.name}</span>
         </div>
-        <box-icon name='trash-alt' type='solid' class='delete' data-task-id="${task.id}" ></box-icon>
+        <box-icon name='trash-alt' type='solid' class='delete icon' data-task-id="${task.id}" ></box-icon>
     </li>
   `;
     tasksDiv.insertAdjacentHTML('afterbegin', listItemElem);
