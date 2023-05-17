dbb7ea4b7293648e773184ac58abded8036d23b4
akezeth <116703083+akezeth@users.noreply.github.com>
2023-05-17 09:49:52 +0000

    Perform add item test

--- addDelete.test.js
+++ addDelete.test.js
@@ -1,31 +1,23 @@
-import addTask from './src/modules/addTask';
-import showTasks from './src/modules/showTasks';
+import addTask from "./src/modules/addTask";
+import showTasks from "./src/modules/showTasks";
+import getTasks from "./src/modules/getTasks";
 
-describe('Tests for Add and Delete Functionality', () => {
-  beforeEach(() => {
-    const listElem = document.createElement('ul');
+describe("Tests for Add and Delete Functionality", () => {
+  test("add one task", () => {
+    const listElem = document.createElement("ul");
     document.body.appendChild(listElem);
-    const dummyTasks = [
-      {
-        id: 1,
-        name: 'Hello World',
-        done: false,
-      },
-      {
-        id: 2,
-        name: 'Hello World 2',
-        done: false,
-      },
-    ];
-    showTasks(listElem, dummyTasks);
-    console.log(listElem);
-  });
-  test('should add an task to local storage and to the page as a list item', () => {
-    const tasks = localStorage.getItem('tasks');
-    expect(tasks).toBeNull();
-    const task = 'Hello World';
+    const task = "Hello World";
     addTask(task);
-    const updatedTasks = localStorage.getItem('tasks');
+    const updatedTasks = getTasks();
+    showTasks(listElem, updatedTasks);
+    expect(listElem.children.length).toBe(localStorage.length);
+  });
+  test("should add an task to local storage and to the page as a list item", () => {
+    const tasks = localStorage.getItem("tasks");
+    expect(tasks).toBeNull();
+    const task = "Hello World";
+    addTask(task);
+    const updatedTasks = localStorage.getItem("tasks");
     expect(updatedTasks).toBe('[{"id":1,"name":"Hello World","done":false}]');
   });
   afterEach(() => {
